7e3ef83902b4e02c3c21cf5c905687a860421853
terakeet-ji <jiverson@terakeet.com>
2021-05-07 09:28:21 -0400

    updated with detailed comments, config file now required not optional

--- send_email.py
+++ send_email.py
@@ -1,37 +1,60 @@
 import smtplib
-from custom_python.make_config import read
-
 from email.mime.text import MIMEText
 
 def send(subject,
          message,
          recipients,
          sender,
-         config_file = None):
+         config_file):
+    '''
+    Sends an email using a gmail account
     
+    Authenticate through a config file you will need an email section
+    with then an email and pwd element
+    
+    ::Variables::
+        
+    subject: str: subject of email message
+    
+    message: list: the body of the email, 
+                    spaces each list element on a new line
+   
+    recipients: list: emails that to receive the email
+   
+    sender: str: formated as email
+   
+    config_file: config file formatted as:
+        
+        {'email':
+         { 'email' : 'example@email.com', 'pwd' : password123 }
+         }
+        
+        create this config file using 
+        configparser or make_config (custom module)
+        and pass it in to authenticate
+        it is recommended to use an APP PASSWORD found here:
+            https://myaccount.google.com/security
+            
+    1) Create the body of the message
+    2) Read in the credentials
+    3) Set up the smtp connnection and login
+    4) Send and close the connection
+    '''
+    #1
     email_txt = MIMEText('\n'.join(message))
     email_txt['Subject'] = subject
     email_txt['From'] = sender
     email_txt['To'] = '\n'.join(recipients)
-
-
-    #THIS pwd is from Google app passwords
-    #you  need this password from your account
-    #read it in through the config file
-    if config_file == None:
-        config_file = read()
-
+    
+    #2
     email = config_file['email']['email']
     pwd = config_file['email']['pwd']
-
+    
+    #3
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
     server.login(email,pwd)
-    server.sendmail('testing@terakeet.com',recipients,email_txt.as_string())
+    
+    #4
+    server.sendmail(sender,recipients,email_txt.as_string())
     server.close()
-
-if __name__ == '__main__':
-    '''
-    send('GBQ',['to be done'],['iverson.jace@gmail.com'],sender = 'me jace')
-    '''
-    pass
